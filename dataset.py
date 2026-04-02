eeb4eac26bd2a4789055c25d40c730f897fdf3da
Hariswar <bkhariswar@gmail.com>
2026-04-02 18:01:18 -0500

    downloaded the dataset

---
+++ dataset.py
@@ -0,0 +1,11 @@
+import kagglehub
+
+# Download latest version
+path_one = kagglehub.dataset_download("aadyasingh55/fake-news-classification")
+
+print("Path to dataset files:", path_one)
+
+# Download latest version
+path_two = kagglehub.dataset_download("saurabhshahane/fake-news-classification")
+
+print("Path to dataset files:", path_two)
